155bc82f548652ad2eecf32263e3dde4c1deb1e0
piqnfikyc <1207Krisi>
2017-05-31 11:15:24 +0300

    Modified Functional Calculator D:\JsCore\JS Fundamentals\SoftUni---JS-Core---2016-\Functions - Lab

--- Functions - Lab/5. Functional Calculator.js
+++ Functions - Lab/5. Functional Calculator.js
@@ -1,10 +1,10 @@
 /**
- *
- * @param num1 - first number{int}
- * @param num2 - second number {int}
- * @param operator {string}
- * @returns value of operation{int}
- */
+*
+* @param num1 - first number{int}
+* @param num2 - second number {int}
+* @param operator {string}
+* @returns value of operation{int}
+*/
 function functionalCalculator(num1,num2,operator) {
     switch(operator){
         case '+': return num1 + num2;
